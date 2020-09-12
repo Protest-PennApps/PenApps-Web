b8333bdd8d95fa92383c22a4da54738d167a4210
Oscar <oscarfbarbosa@gmail.com>
2020-09-12 16:40:49 -0400

    fixed css styling

--- src/components/navigation/Navigation.js
+++ src/components/navigation/Navigation.js
@@ -5,9 +5,9 @@ const Navigation = () => {
     return (
         <nav style={{display: 'flex', justifyContent: 'flex-end', paddingTop: '5x'}}>
             <ul className ="main-nav">
-                <li><a href="/">HOME</a></li>
-                <li><a href="/">EVENTS</a></li>
-                <li><a href="/">ORGANIZE</a></li>
+                <li className= "nav-list"><a className="nav" href="/">HOME</a></li>
+                <li className= "nav-list"><a className="nav" href="/">EVENTS</a></li>
+                <li className= "nav-list"><a className="nav" href="/">ORGANIZE</a></li>
             </ul>
         </nav>
     );
