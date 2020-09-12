bdb4b029aec0730f0726193aa10e6ada8878a3c3
Oscar <oscarfbarbosa@gmail.com>
2020-09-12 15:02:57 -0400

    Added header

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 import Home from './pages/home';
+import Header from './components/Header';
+import Logo from './components/logo/Logo';
+import 'tachyons';
 
 function App() {
   return (
     <Router>
-      <Route exact path='/' component={ Home } />
+      <Route exact path='/' component={Header} />
     </Router>
   );
 }
