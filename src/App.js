c336092acd99124a046c633a7754f74e8fa0841d
Tommy Carrascal <carrascalx@gmail.com>
2020-09-12 15:33:05 -0400

    initial IU draft done

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 import Home from './pages/home';
+import Event from './pages/event';
 
 function App() {
   return (
     <Router>
       <Route exact path='/' component={ Home } />
+      <Route exact path='/event' component={ Event } />
     </Router>
   );
 }
